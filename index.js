024d50e3f0dc75712b640dd8414765711873b2af
Yukun Liu <yukunliu@YukundeMacBook-Pro.local>
2017-09-13 13:55:42 -0700

    test

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
+app.post('/test', function(request, response) {
+  response.send(cool());
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
