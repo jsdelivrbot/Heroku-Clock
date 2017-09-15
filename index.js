545a0c05265e97f0898506ec0a67222f361e6a91
Yukun Liu <yukunliu@YukundeMacBook-Pro.local>
2017-09-15 11:46:06 -0700

    fix cluster

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const throng = require('throng');
 const cluster = require('cluster');
 
 const WORKERS = process.env.WEB_CONCURRENCY || 1;
-for (let i = 0; i < WORKERS; i++) {
-  cluster.fork();
+if(cluster.isMaster) {
+  for (let i = 0; i < WORKERS; i++) {
+    cluster.fork();
+  }
 }
 
 throng({
